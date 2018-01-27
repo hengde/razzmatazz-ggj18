07b12709f9f2c172962d27268902b6144b4b5a41
Chris Wallace <csw339@nyu.edu>
2018-01-26 20:43:26 -0500

    Microphone works

---
+++ Assets/Scripts/MicrophoneTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneTest : MonoBehaviour {
+
+	List<AudioClip> clips;
+	AudioSource aSource;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(Microphone.devices[0]);
+		clips = new List<AudioClip>();
+		aSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!Microphone.IsRecording(Microphone.devices[0]) && Input.GetKeyDown(KeyCode.Space)){
+			AudioClip newClip = new AudioClip();
+			newClip = Microphone.Start(Microphone.devices[0],false,5, 44100);
+			clips.Add(newClip);
+		} else if(Input.GetKeyDown(KeyCode.Space)) {
+			Microphone.End(Microphone.devices[0]);
+		}
+
+		if(Input.GetKeyDown(KeyCode.P)){
+			aSource.clip = clips[clips.Count - 1];
+			aSource.Play();
+		}
+
+	}
+}
