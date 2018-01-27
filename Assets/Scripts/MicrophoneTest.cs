f29bcd6f7fa59f23bddc875d1a4eb0acb5aa35af
Nick <nicholasJmaddalena@gmail.com>
2018-01-27 11:11:03 -0500

    some code for detected loudness from spectrum data

--- Assets/Scripts/MicrophoneTest.cs
+++ Assets/Scripts/MicrophoneTest.cs
@@ -6,16 +6,32 @@ public class MicrophoneTest : MonoBehaviour {
 
 	List<AudioClip> clips;
 	AudioSource aSource;
+	public bool IsRecording;
+	public bool IsLoud;
+	public float LoudnessThreshhold;
+
+	public LineRenderer MyLineRenderer;
+	private float[] SpectrumDataValues;
+	private Vector3[] positions;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log(Microphone.devices[0]);
+		foreach(string s in Microphone.devices){
+			Debug.Log(s);
+		}
 		clips = new List<AudioClip>();
 		aSource = GetComponent<AudioSource>();
+		SpectrumDataValues = new float[512];
+		positions = new Vector3[100];
+		for(int i = 0; i < positions.Length; i++){
+			positions[i] = new Vector3(i*.15f,0,0);
+		}
+		MyLineRenderer.positionCount = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		IsRecording = Microphone.IsRecording(Microphone.devices[0]);
 		if(!Microphone.IsRecording(Microphone.devices[0]) && Input.GetKeyDown(KeyCode.Space)){
 			AudioClip newClip = new AudioClip();
 			newClip = Microphone.Start(Microphone.devices[0],false,5, 44100);
@@ -27,6 +43,18 @@ public class MicrophoneTest : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.P)){
 			aSource.clip = clips[clips.Count - 1];
 			aSource.Play();
+		}
+
+		if(aSource.isPlaying){
+			aSource.GetSpectrumData(SpectrumDataValues,0,FFTWindow.Blackman);
+			IsLoud = false;
+			for(int i = 0; i < positions.Length; i++){
+				positions[i].y = SpectrumDataValues[i] * 20;
+				if (SpectrumDataValues[i] > LoudnessThreshhold){
+					IsLoud = true;
+				}
+			}
+			MyLineRenderer.SetPositions(positions);
 		}
 
 	}
