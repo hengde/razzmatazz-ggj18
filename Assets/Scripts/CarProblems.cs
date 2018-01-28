611a9387ea027b1eeb9b43d52ae426cda8182ab0
Nick <nicholasJmaddalena@gmail.com>
2018-01-27 22:09:36 -0500

    added array of strings for funny sound

--- Assets/Scripts/CarProblems.cs
+++ Assets/Scripts/CarProblems.cs
@@ -22,9 +22,64 @@ public static class CarProblems{
         "Activate", "spark", "rocket",
     };
 
+    public static string[] funnySoundSolutions = new string[] {
+        "Supercharge", "subatomic", "synthesizer",
+        "Bedazzle", "loadbearing", "mixer",
+        "Prime", "overworked", "motor", 
+        "Reheat", "antigravity", "gate",
+        "Tighten", "spinning", "pedal", 
+        "Reinstall", "oriented", "pulley",
+        "Defrost", "manual", "lever",
+        "Grind", "functional", "port",
+        "Magnify", "nuclear", "wheel",
+        "Appreciate", "organic", "cable",
+        "Unscramble", "negative", "positron",
+        "Resize", "derivative", "transporter",
+        "Configure", "supersonic", "lockbox", 
+        "Cool", "crank", "pipe",
+        "Beautify", "ultra", "bolt",
+        "Diversify", "mega", "cord", 
+        "Enchant", "audiovisual", "lobe",
+        "Encrypt", "optic", "switch"
+    };
+
     public static string[] GetKeywordsForPartProblem(int row, int col, bool b0, bool b1, bool b2){
         string[] retStrings = new string[3];
+        int indexInChart = getIndexInChart(row, col, b0, b1, b2);
+        retStrings[0] = brokenPartSolutions[indexInChart];
+        retStrings[1] = brokenPartSolutions[indexInChart+1];
+        retStrings[2] = brokenPartSolutions[indexInChart+2];
+        return retStrings;
+    }
 
+    public static string[] GetKeywordsForSoundProblem(int row, int col, bool b0, bool b1, bool b2){
+        string[] retStrings = new string[3];
+        int indexInChart = getIndexInChart(row, col, b0, b1, b2);
+        retStrings[0] = funnySoundSolutions[indexInChart];
+        retStrings[1] = funnySoundSolutions[indexInChart+1];
+        retStrings[2] = funnySoundSolutions[indexInChart+2];
+        return retStrings;
+    }
+
+    public static string[] AllKeywords(){
+        string[] allStrings = new string[
+            brokenPartSolutions.Length + 
+            funnySoundSolutions.Length
+        ];
+
+        int j = 0;
+        for(int i = 0; i < brokenPartSolutions.Length; i++){
+            allStrings[i+j] = brokenPartSolutions[i];
+        };
+        j += brokenPartSolutions.Length;
+
+        for(int i = 0; i < funnySoundSolutions.Length; i++){
+            allStrings[i+j] = funnySoundSolutions[i];
+        };
+        j += funnySoundSolutions.Length;
+    }
+
+    static int getIndexInChart(int row, int col, bool b0, bool b1, bool b2){
         bool boolForColumn = false;
         if (col == 0){
             boolForColumn = b0;
@@ -39,16 +94,6 @@ public static class CarProblems{
         int boolOffset = boolForColumn ? 0 : 1;
         int indexInChart = (row*6) + (col*2) + boolOffset;
         indexInChart *= 3;
-
-        retStrings[0] = brokenPartSolutions[indexInChart];
-        retStrings[1] = brokenPartSolutions[indexInChart+1];
-        retStrings[2] = brokenPartSolutions[indexInChart+2];
-
-        return retStrings;
-    }
-
-
-    public static string[] AllKeywords(){
-        return brokenPartSolutions;
+        return indexInChart;
     }
 }
