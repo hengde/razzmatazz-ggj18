98a4b8505a15234a0bb8ccec7c68cded37c04db6
Nick <nicholasJmaddalena@gmail.com>
2018-01-28 14:49:31 -0500

    killed untitled cs

--- Assets/Scripts/Task/Untitled.cs
+++ Assets/Scripts/Task/Untitled.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿// using System;
 
 
-class Unititled{
-	static void Main(string[]args){
-		TaskManager tm = new TaskManager();
+// class Unititled{
+// 	static void Main(string[]args){
+// 		TaskManager tm = new TaskManager();
 
-		tm.Do(()=>(Console.WriteLine("Hello"))).Then
-		(new WaitTask(1000)).Then
-		(new ActionTask(()=>(Console.WriteLine("Goodbye"))));
+// 		tm.Do(()=>(Console.WriteLine("Hello"))).Then
+// 		(new WaitTask(1000)).Then
+// 		(new ActionTask(()=>(Console.WriteLine("Goodbye"))));
 
-		while(true){
-			tm.Update();
-		}
+// 		while(true){
+// 			tm.Update();
+// 		}
 
-	}
+// 	}
 
-}
+// }
