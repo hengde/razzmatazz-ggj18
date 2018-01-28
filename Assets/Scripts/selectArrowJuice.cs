a1874b32ced77f89cc7aa152fa4f01eb3e45ba7c
Nick <nicholasJmaddalena@gmail.com>
2018-01-28 15:34:39 -0500

    fancy spectrumData

--- Assets/Scripts/selectArrowJuice.cs
+++ Assets/Scripts/selectArrowJuice.cs
@@ -4,50 +4,20 @@ using UnityEngine.UI;
 using System;
 
 public class selectArrowJuice : MonoBehaviour {
-	bool growing;
-	public float MAX_SIZE = 1.1f;
-	public float MIN_SIZE = .7f;
-	public float GROWTH_RATE = .01f;
-	public bool stopped;
+	
+	public AudioSource CarAudio;
+	private float[] samples;
 
 	// Use this for initialization
 	void Start () {
-		growing = true;
-		//stopped = false;
+		samples = new float[512];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(!stopped){
-			try{
-			} catch(Exception e){
-
-			}
-			if(growing){
-				//increase size by frame
-				transform.localScale = new Vector3 (transform.localScale.x + GROWTH_RATE, transform.localScale.y + GROWTH_RATE, transform.localScale.z);
-
-				if(transform.localScale.x > MAX_SIZE){
-					growing=false;
-				}
-			} else {
-				//decrease size by frame
-				transform.localScale = new Vector3 (transform.localScale.x - GROWTH_RATE, transform.localScale.y - GROWTH_RATE, transform.localScale.z);
-
-				if(transform.localScale.x < MIN_SIZE){
-					growing=true;
-				}
-			}
+		if (CarAudio.isPlaying){
+			CarAudio.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+			transform.localScale = (Vector3.one*7) + (Vector3.one * Mathf.Min(samples[50], .125f) * 125);
 		}
-		else {
-			try{
-			} catch(Exception e){
-			}
-		}
-
-	}
-
-	public void stopMotion(){
-		stopped = true;
 	}
 }
